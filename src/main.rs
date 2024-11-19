4b325c22e71e1f4925f25a82352b5902231fbd85
data <K1NGKAOS100@Gmail.com>
2024-11-19 03:56:27 -0800

    improved error checking, added word/phrase filters

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::fs::OpenOptions;
 use std::io::Read;
+use std::fs::write;
+use std::path::Path;
 use std::env;
 use std::process;
 
-use matrix_sdk::ruma::events::room::message::RoomMessageEvent;
-use matrix_sdk::ruma::events::room::name::RoomNameEventContent;
-use matrix_sdk::ruma::events::StaticEventContent;
-
+use matrix_sdk::ruma::api::client::session;
+use matrix_sdk::HttpError;
 use url::Url;
 use toml::from_str;
 use log_x::{loggers::{ global_logger::DefaultLoggerTrait, log_levels::LogLevel }, 
                        timestamp, log_info, log_error, log_warn, log_debug, Logger};
-
 mod config;
 
+use matrix_sdk::matrix_auth::MatrixSession;
+use matrix_sdk::matrix_auth::MatrixSessionTokens;
+use matrix_sdk::SessionMeta;
 use matrix_sdk::{
     config::SyncSettings,
+    ruma::device_id,
+    ruma::user_id,
     ruma::events::room::message::{MessageType, SyncRoomMessageEvent},
-    Client, Room, RoomState,
+    Client, Room,
 };
 
 #[tokio::main]
@@ -25,16 +29,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Logger::set_log_level(LogLevel::Trace);
     let path = env::current_dir()?;
     let mut file = match OpenOptions::new().read(true).open("config.toml") {
-       Ok(file) => file,
-       Err(NotFound) => {
-            log_info!("Configuration file not found, creating one in directory of executable...");
-            config::write_config_defaults()?;
-            log_info!("Default configuration file created in : {} , you will have to configure it.", path.display());
-            print!("");
-            OpenOptions::new().read(true).open("config.toml")?
-        },
-    };
-
+         Ok(file) => file,
+         Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+             log_info!("Configuration file not found, creating one in directory of executable...");
+             config::write_config_defaults()?;
+             log_info!("Default configuration file created in : {} , you will have to configure it.", path.display());
+             print!("");
+             process::exit(0x0100);
+         },
+         Err(e) => return Err(e.into()),
+     };
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let tables: config::Tables = from_str(&contents)?; //accessible struct for values
@@ -49,21 +53,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if tables.info.room_id == "example-room-id:matrix.org" {
         log_error!("Room ID for automatic moderation is not set in the configuration file {}/config.toml, please configure it.", path.display());
     }
-
+    
     let homeserver_url = Url::parse(&format!("https://{}", tables.login.username.split(':').nth(1).unwrap()))?;
     let client = Client::new(homeserver_url).await?;
 
+    
     match client.matrix_auth().login_username(&tables.login.username, &tables.login.password).send().await {
         Ok(_) => {
             log_info!("Logged in as {}", &tables.login.username);
         }
-        
+            
         Err(error) => {
             log_error!("{}", error);
         }
     }
-
+    
     client.add_event_handler(|ev: SyncRoomMessageEvent, room: Room| async move {
+        println!("---");
         if let SyncRoomMessageEvent::Original(orig) = ev { // gets the Original 
             if let MessageType::Text(text_content) = orig.content.msgtype {
                 if tables.info.room_id == room.room_id().to_string() {
@@ -76,6 +82,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     log_debug!("Body: {}", text_content.body);
                     println!("\n");
                 }
+
+                let substr_match = tables.filters.message_filters.iter().any(|s| text_content.body.contains(s));
+                if substr_match {
+                    println!("Banning user {}", orig.sender.to_string());
+                    match room.ban_user(&orig.sender, None).await {
+                        Ok(_) => {
+                            log_info!("User {} banned", orig.sender.to_string());
+                        }
+                        Err(error) => {
+                            log_error!("{}", error);
+                        }
+                    }
+                }
             }
             else {
                 log_error!("SyncRoomMessageEvent::Original doesn't contain a MessageType::Text");
@@ -85,7 +104,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             log_error!("message event doesn't have an original, this should never happen.");
         }
     });
+
     client.sync(SyncSettings::default()).await?;
-    
     Ok(())
 }
